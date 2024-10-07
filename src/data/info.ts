1ef6bb531d4bab0337ea0aa041e12ec8de2272d7
farid <souroufaridadoi0@gmail.com>
2024-10-07 17:34:01 +0100

    First portfolio with astro.js

---
+++ src/data/info.ts
@@ -0,0 +1,98 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Farid ADOI",
+  jobDescription: "Frontend Developer",
+  about: `I'm a Frontend engineer based in Cotonou, Benin.
+
+  I'm a frontend developer who likes to play and draw with colors in code. My goal is to provide the
+  most beautiful and easy-to-use interfaces for startups' websites and applications. I've been working
+  towards this goal for the past three (03) years on a number of projects that I invite you to discover.
+   The technologies I masterinclude HTML, CSS, JavaScript, Bootstrap, tailwind CSS, Laravel, Wordpress, Vue JS and Electron JS. Hence,
+   I have successfully completed several web projects from scratch. 
+   Yet, I learn continuously and never stop exploring new technologies.
+   So, I have also gained knowledge of Astro JS, SCSS and more. 
+  `,
+
+  experience: [
+    {
+      name: "Figurine France Manga",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "Oct 2022",
+      endDate: "Fev 2024",
+      description: [
+        "- ",
+        "- ",
+      ],
+    },
+
+    {
+      name: "Coupon Approve (Remote)",
+      location: "Amsterdam, North Holland, Netherlands",
+      startDate: "Nov 2020",
+      endDate: "Apr 2022",
+      description: [
+        "✓ Developing code in accordance with appropriate design and architectural rules",
+        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
+        "✓ Help and training of other colleagues",
+        "✓ Others tasks connected to the nature of the job",
+        "✓ Code Improvements and Refactoring ",
+        "✓ Best Practices and Rules Maintainer",
+      ],
+    },
+
+    {
+      name: "Interns Pakistan",
+      location: "Pakistan",
+      startDate: "Oct 2020",
+      endDate: "Nov 2020",
+      description: [
+        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
+        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "2019",
+      endDate: "2022",
+      description: [
+        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+      ],
+    },
+    {
+      name: "Govt. Degree Boys College Buffer Zone",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "2018",
+      endDate: "2020",
+      description: ["Intermediate, Pre-Engineering"],
+    },
+    {
+      name: "Ghazi Foundation Schools & Colleges",
+      location: "Karāchi, Sindh, Pakistan",
+      startDate: "2016",
+      endDate: "2018",
+      description: ["Matriculation,  Computer Science"],
+    },
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
+    twitter: "https://twitter.com/Uzzii_21",
+    github: "https://github.com/uzzii-21",
+    email: "mailto:uzairahmed@74372gmail.com",
+    linkedin: "https://www.linkedin.com/in/uzzii21/",
+  },
+
+  projects: [
+    {
+      title: "Astro Portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+  ],
+};
