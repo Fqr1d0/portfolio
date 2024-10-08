4002211108caf42fdebb163879b393d32e9f0d35
farid <souroufaridadoi0@gmail.com>
2024-10-08 08:47:35 +0100

    Modifying my bio and some scriptural details

--- src/data/info.ts
+++ src/data/info.ts
@@ -7,8 +7,8 @@ export const info = {
   Je suis un développeur frontend qui aime jouer et dessiner avec des couleurs dans le code. Mon objectif est de fournir les
   interfaces les plus belles et les plus faciles à utiliser pour les sites web et les applications des startups. J'ai travaillé
   depuis trois (03) ans sur plusieurs projets que je vous invite à découvrir.
-   Les technologies que je maîtrise comprennent : HTML, CSS, JavaScript, Bootstrap, tailwind CSS, Laravel, Wordpress, GSAP, Jquery, Vue JS et Electron JS. D'où,
-   j'ai mené à bien plusieurs projets web en partant de zéro. 
+   Les technologies que je maîtrise comprennent : HTML, CSS, JavaScript, Bootstrap, tailwind CSS, Laravel, Wordpress, GSAP, Jquery, Vue JS et Electron JS. Avec ces technos,
+   j'ai mené à bien plusieurs projets web "from scratch". 
    Cependant, j'apprends continuellement et je n'arrête jamais d'explorer de nouvelles technologies.
    Ainsi, j'ai également acquis des connaissances en Astro JS, SCSS et plus encore.`,
 
@@ -28,7 +28,7 @@ export const info = {
       name: "RAYNIS - Chargé de communication adjoint ",
       location: "Cotonou, Bénin, Présentiel",
       startDate: "Nov 2020",
-      endDate: "Apr 2022",
+      endDate: "Avr 2022",
       description: [
         "✓ Suivi des interactions dans le groupe et sur la page de l'entreprise",
         "✓ Contact avec les utilisateurs de l'application RMobility sur le terrain",
@@ -51,7 +51,7 @@ export const info = {
     {
       name: "Coding Academy by EPITECH",
       location: "EPITECH Bénin, Cotonou, Bénin",
-      startDate: "Jun 2024",
+      startDate: "Jui 2024",
       endDate: "-",
       description: [
         "Développement web front-end",
